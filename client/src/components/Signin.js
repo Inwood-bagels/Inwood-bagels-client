f953ba496be5ff1d7c90ebaf877888ea84b44be9
hemaswi <hemaswi.gorrela@gmail.com>
2021-12-11 14:12:01 -0500

    sign-in styling

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react'
-import {Link, useHistory} from 'react-router-dom'
-import {auth, signInWithEmailAndPassword, signInWithGoogle} from '../firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from '../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import '../styles/Signin.css'
 
 function Signin() {
@@ -23,33 +23,39 @@ function Signin() {
     return (
         <div className="signin">
             <div className="signin_container">
-                Email Address
-                <input
-                    type="text"
-                    className="signin_textBox"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="E-mail Address"
-                />
-                Password
-                <input
-                    type="password"
-                    className="signin_textBox"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                />
+                <div className='credentials'>
+                    <div className='email_box'>
+                        Email Address
+                        <input
+                            type="text"
+                            className="signin_textBox"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="E-mail Address"
+                        />
+                    </div>
+                    <div className='password_box'>
+                        Password
+                        <input
+                            type="password"
+                            className="signin_textBox"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </div>
+                </div>
                 <button
                     className="signin_btn"
                     onClick={() => signInWithEmailAndPassword(email, password)}
                 >
-                    Login
+                    Sign in
                 </button>
                 <button className="signin_btn signin_google" onClick={signInWithGoogle}>
-                    Login with Google
+                    Sign in with Google
                 </button>
                 <div>
-                    Forgat Password? <Link to="/reset">Click here to reset password</Link>
+                    Forgot Password? <Link to="/reset">Click here to reset password</Link>
                 </div>
                 <div>
                     Don't have an account? <Link to="/signup">Sign up here</Link>
