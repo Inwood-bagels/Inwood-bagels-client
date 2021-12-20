99afa8914115222e90411c186ac0bee4cf7470ae
hemaswi <hemaswi.gorrela@gmail.com>
2021-12-20 12:42:13 -0500

    signup styling

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -33,88 +33,99 @@ function Signup() {
 
     return (
         <div className="signup">
-            <div className="signup_container">
-                All items marked with a * are required
-                <div>
-                    <div>
-                        Name * {}
-                        <input
-                            type="text"
-                            className="signup_textBox"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="Full Name"
-                        />
+            <div className="signup_container">                
+                <div className='signup_box'>
+                    <div className='details_address'>
+                        <div className='user_details'>
+                            <h1>All items marked with a * are required</h1>
+                            <div className='fill_in'>
+                                Name * {}
+                                <input
+                                    type="text"
+                                    className="signup_textBox"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Full Name"
+                                />
+                            </div>
+                            <div className='fill_in'> 
+                                Email Address *
+                                <input
+                                    type="text"
+                                    className="signup_textBox"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="E-mail Address"
+                                />
+                            </div>
+                            <div className='fill_in'>
+                                Password *
+                                <input
+                                    type="password"
+                                    className="signup_textBox"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div className='fill_in'>
+                                Re-enter Password *
+                                <input
+                                    type="password"
+                                    className="signup_textBox"
+                                    value={checkPassword}
+                                    onChange={(e) => setCheckPassword(e.target.value)}
+                                    placeholder="Re-enter Password"
+                                />
+                            </div>
+                        </div>
+                        
+                        <div className='address'>
+                            <h2>Delivery Address</h2>
+                                <div className='address_column'>
+                                    <p className='address_fill'>Address</p>
+                                    <input
+                                        type="text"
+                                        className="signup_textBox"
+                                        value={address}
+                                        onChange={(e) => setAddress(e.target.value)}
+                                        placeholder="Address"
+                                    />
+                                </div>
+                                <div className='address_column'> 
+                                    <p className='address_fill'>City</p>
+                                    <input
+                                        type="text"
+                                        className="signup_textBox"
+                                        value={city}
+                                        onChange={(e) => setCity(e.target.value)}
+                                        placeholder="City"
+                                    />
+                                </div>
+                                <div className='address_column'>
+                                    <p className='address_fill'> State </p>
+                                    <input
+                                        type="text"
+                                        className="signup_textBox"
+                                        value={stateUs}
+                                        onChange={(e) => setStateUs(e.target.value)}
+                                        placeholder="State"
+                                    />
+                                </div>
+                                <div className='address_column'>
+                                    <p className='address_fill'> Zip Code </p>
+                                    <input
+                                        type="zip"
+                                        className="signup_textBox"
+                                        value={zip}
+                                        onChange={(e) => setZip(e.target.value)}
+                                        placeholder="Zip Code"
+                                    />
+                                </div>
+                        </div>
                     </div>
-
-                    <div> Email Address *
-                        <input
-                            type="text"
-                            className="signup_textBox"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="E-mail Address"
-                        /></div>
-                    <div>
-                        Password *
-                        <input
-                            type="password"
-                            className="signup_textBox"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div>
-                        Re-enter Password *
-                        <input
-                            type="password"
-                            className="signup_textBox"
-                            value={checkPassword}
-                            onChange={(e) => setCheckPassword(e.target.value)}
-                            placeholder="Re-enter Password"
-                        />
-                    </div>
-                    Delivery Address
-                    <div>
-                        Address
-                        <input
-                            type="text"
-                            className="signup_textBox"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                            placeholder="Address"
-                        />
-                    </div>
-
-                    <div> City
-                        <input
-                            type="text"
-                            className="signup_textBox"
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                            placeholder="City"
-                        /></div>
-                    <div>
-                        State
-                        <input
-                            type="text"
-                            className="signup_textBox"
-                            value={stateUs}
-                            onChange={(e) => setStateUs(e.target.value)}
-                            placeholder="State"
-                        />
-                    </div>
-                    <div>
-                        Zip Code
-                        <input
-                            type="zip"
-                            className="signup_textBox"
-                            value={zip}
-                            onChange={(e) => setZip(e.target.value)}
-                            placeholder="Zip Code"
-                        />
-                    </div>
+                    
+                    
                     <div>
                         <button className="signup_btn" onClick={register}>
                             Sign up
