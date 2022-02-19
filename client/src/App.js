692bf93f23a83ff9ab8f2b994c86168b696d0f58
hemaswi <hemaswi.gorrela@gmail.com>
2022-02-19 11:48:50 -0500

    reset password page

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Signup from './components/Signup'
 import Home from "./components/Home"
 import Footer from "./components/Footer"
 import Profile from "./components/Profile"
+import Resetpassword from './components/ResetPassword'
 
 const divStyle = {
     backgroundImage: 'url("https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/255773017_4583362898373572_1745065988058525732_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=qZZaJQhsjwMAX9PTfOu&_nc_ht=scontent-lga3-1.xx&oh=3eddf9e82ed2dbfa985bce7cacf59bf6&oe=619245E6)',
@@ -28,6 +29,7 @@ function App() {
                     <Route exact path="/location" component={Location}/>
                     <Route path="/order" component={Order}/>
                     <Route path="/signin" component={Signin}/>
+                    <Route exact path="/resetpassword" component={Resetpassword}/>
                     <Route exact path="/signup" component={Signup}/>
                     <Route path="/settings" component={Profile}/>
                 </Switch>
