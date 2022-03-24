152e498544ca74af7024cf4af632ef9ffa3f2733
Gaelle Gilles <67564107+gia534@users.noreply.github.com>
2022-03-24 09:14:29 -0400

    Updates to admin files

--- client/src/components/AdminAccount.js
+++ client/src/components/AdminAccount.js
@@ -1,39 +1,8 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import '../styles/styles.css'
-import {Link, useHistory} from 'react-router-dom'
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth, db} from "../firebase";
+import { Link } from 'react-router-dom'
 
 function AdminAccount() {
-    const [email, setEmail] = useState("")
-    const [name, setName] = useState("")
-    const[role, setRole] = useState("")
-    const [admin, loading, error] = useAuthState(auth)
-    const history = useHistory()
-
-    const fetchAdmin = async () => {
-        try{
-            const query = await db
-                .collection("Admin")
-                .where("uid", "==", admin?.uid)
-                .get();
-            const data = await query.docs[0].data();
-            setName(data.name);
-            setEmail(data.email);
-
-
-        }catch (error){
-            console.error(error);
-            alert("An error has occurred while fetching the user's data")
-        }
-    };
-
-    useEffect(() => {
-        if(loading) return;
-        if(!admin) return history.replace("/");
-        fetchAdmin();
-
-    }, [admin, loading]);
 
     return (
         <div className='admin'>
@@ -42,22 +11,10 @@ function AdminAccount() {
                 <div className='admin_acc'>
                     <div className='name_email'>
                         <div className='adminame'>
-                            Employee Name:
-                            <input
-                                type="text"
-                                value={name}
-                                placeholder={admin.name}
-                                />
-
+                            Employee Name: 
                         </div>
                         <div className='adminemail'>
-                            Email Address:
-                            <input
-                                type="text"
-                                value={email}
-                                placeholder={email}
-                            />
-
+                            Email Address: 
                         </div>
                     </div>
                     <div className='role_pswrd'>
